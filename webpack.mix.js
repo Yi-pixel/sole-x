0e2f8fadc758dc5609f903d3871b4b945f0433d9
Yi <m@2vy.cc>
2021-02-21 21:19:51 +0800

    [新增功能](0.2.1): 添加前端脚手架

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js(['resources/js/app.js', 'vendor/sole-x/blog/src/resources/js/app.js'], 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+
+if (mix.inProduction()) {
+    mix.version()
+}
